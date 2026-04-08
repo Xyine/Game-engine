745813c6140fe1fe6adaab6541b61a295187aa63
Cyrius <desagazancyrius@gmail.com>
2026-04-08 11:37:28 +0200

    feat: begin loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+
+void runEngine();
+
+
 int main() {
-    std::cout << "Hello, Engine!\n";
+    runEngine();
     return 0;
+}
+
+
+void runEngine() {
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Engine running...\n";
+    }
 }
