1a8b385bc5093620f54f8bab1c12159f21fe39cc
Cyrius <desagazancyrius@gmail.com>
2026-04-12 15:32:56 +0200

    feat: acceleration and boundary

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ const int objectCount = 2;
 
 struct GameObject;
 
-void updateSystem(GameObject& gameObject, float deltaTime);
+void movementSystem(GameObject& gameObject, float deltaTime);
+void physicsSystem(GameObject& gameObject, float deltaTime);
+void boundarySystem(GameObject& gameObject, float minX, float maxX, float minY, float maxY);
 void renderSystem(const GameObject& gameObject);
 
 struct Vec2 {
@@ -26,14 +28,19 @@ struct GameObject {
     const char* name;
     Vec2 position;
     Vec2 velocity;
+    Vec2 acceleration;
 
     void print() const {
         std::cout << name << " position: ";
         position.print();
+        std::cout << ", velocity: ";
+        velocity.print();
+        std::cout << ", acceleration: ";
+        acceleration.print();
         std::cout << "\n";
     }
 
-    void update(float deltaTime) { position.add(velocity, deltaTime); }
+    void move(float deltaTime) { position.add(velocity, deltaTime); }
 };
 
 struct Engine {
@@ -41,11 +48,15 @@ struct Engine {
     int frame;      // current frame count
     int maxFrames;  // configuration
     float deltaTime;
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
     std::array<GameObject, objectCount> objects;
     
-    Engine() : isRunning(true), frame(0), maxFrames(3), deltaTime(0.5f) {
-        objects[0] = {"Player", {0.0f, 0.0f}, {1.0f, 0.0f}};
-        objects[1] = {"Enemy", {10.0f, 5.0f}, {0.0f, 1.0f}};
+    Engine() : isRunning(true), frame(0), maxFrames(3), deltaTime(0.5f), minX(0.0f), maxX(3.0f), minY(0.0f), maxY(6.0f) {
+        objects[0] = {"Player", {0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 0.0f}};
+        objects[1] = {"Enemy", {10.0f, 5.0f}, {0.0f, 1.0f}, {0.0f, 0.0f}};
     }
 
     void updateState() {
@@ -66,7 +77,9 @@ struct Engine {
 
     void updateObjects() {
         for (GameObject& object : objects) {
-            updateSystem(object, deltaTime);
+            physicsSystem(object, deltaTime);
+            movementSystem(object, deltaTime);
+            boundarySystem(object, minX, maxX, minY, maxY);
         }
     }
 
@@ -102,8 +115,29 @@ int main() {
     return 0;
 }
 
-void updateSystem(GameObject& gameObject, float deltaTime) {
-    gameObject.update(deltaTime);
+void movementSystem(GameObject& gameObject, float deltaTime) {
+    gameObject.move(deltaTime);
+}
+
+void physicsSystem(GameObject& gameObject, float deltaTime) {
+    gameObject.velocity.add(gameObject.acceleration, deltaTime);
+}
+
+void boundarySystem(GameObject& gameObject, float minX, float maxX, float minY, float maxY) {
+    if (gameObject.position.x > maxX) {
+        gameObject.position.x = maxX;
+        gameObject.velocity.x = -gameObject.velocity.x;
+    } else if (gameObject.position.x < minX) {
+        gameObject.position.x = minX;
+        gameObject.velocity.x = -gameObject.velocity.x;
+    }
+    if (gameObject.position.y > maxY) {
+        gameObject.position.y = maxY;
+        gameObject.velocity.y = -gameObject.velocity.y;
+    } else if (gameObject.position.y < minY) {
+        gameObject.position.y = minY;
+        gameObject.velocity.y = -gameObject.velocity.y;
+    }
 }
 
 void renderSystem(const GameObject& gameObject) {
